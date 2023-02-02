52fd4c856233ad86fab3428900133881034a96fe
Alex Gura <aagura@yandex.ru>
2023-02-02 19:44:12 +0300

    Написал код домашки семинара 3

---
+++ T1/Program.cs
@@ -0,0 +1,63 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result)) 
+        {
+        if (Math.Abs (result) < 9999 || Math.Abs (result) > 99999)     
+           {Console.WriteLine("Ввели не пятизначное число. Повторите ввод");
+           }
+           else 
+           {
+            break;
+           }
+        }
+
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+
+int reverse (int number)
+{
+int rev = 0;
+int dig = 1;
+number = Math.Abs (number);
+while(number  > 0)
+  { 
+    dig =number  % 10;
+    rev= rev * 10 + dig;
+    number = number / 10;
+  }
+  return rev;
+}
+    
+int number = GetNumber ("Введите пятизначное число");
+
+int polindrom = reverse (number);
+
+if(Math.Abs (polindrom) == Math.Abs (number))
+{
+   Console.WriteLine ("Это палиндром!");
+}
+else
+{
+  Console.WriteLine ("Это не палиндром!");
+}
---
+++ T3/Program.cs
@@ -0,0 +1,41 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result)) 
+        {
+            break;
+        }
+        
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+void cub (int number)
+{
+ int i = 1;
+  while (i<number)
+  {
+    Console.Write (Convert.ToString (Math.Pow (i,3)));
+    Console.Write (", ");
+    i++;
+  }
+    Console.Write (Convert.ToString (Math.Pow (number,3)));
+}
+    
+int number = GetNumber ("Введите  число");
+cub (number);
